52e099131553810d8c67c6f8f0b54c52dfa93880
Mariam-asiimwe <m.asiimwe@outlook.com>
2023-01-08 16:05:11 +0000

    added codes to prompt and validate password length and password to include at least one of each character in the given arrays.

--- script.js
+++ script.js
@@ -92,52 +92,129 @@ let upperCasedCharacters = [
 ];
 console.log(upperCasedCharacters)
 
-let passwordOptionsArray = [];
+let newCharactersArray = passwordOptionsArray
+//let passwordOptionsArray = passwordOptionsArray.concat(numericCharacters,specialCharacters, upperCasedCharacters, lowerCasedCharacters);
+console.log(passwordOptionsArray)
 
-passwordOptionsArray = passwordOptionsArray.concat(specialCharacters,numericCharacters, lowerCasedCharacters, upperCasedCharacters)
-
-//function passwordOptionArray(){
-     
+//function passwordOptionArray(){  
   //return passwordOptionArray
+//let passwordOptionsArray = passwordOptionsArray.concat(numericCharacter, specialCharacter, upperCasedCharacter, lowerCasedCharacter) 
+//console.log(passwordOptionsArray)
+  
+  let passwordLength = "True"
+  
+  let password = {
+    //Length query
+    length: 0,
+    specialCharacters: [],
+    lowerCasedCharacters: [],
+    upperCasedCharacters: [],
+    numericCharacters: [],
+    //passwordOptionsArray: "",
+    
+  }
+  if (passwordLength.password === "True") {
+    console.log("Is long enough")
+    
+  } else {
+    console.log("Not long enough")
+  }
+  
+  
+  
 
-console.log(passwordOptionsArray);
-
-// Function to prompt user for password options
+//choice of length of password at least 10 character but no more than 64
 function getPasswordOptions() {
-  passwordOptionsArray = passwordOptionsArray.concat(exampleArray);
-
-  return passwordOptionsArray
+  let passwordLength = password.length()
+  //for (let i = 0; i < passwordOptionsArray.length; i++); 
+  //length += passwordOptionsArray[i];
   
 }
+console.log(getPasswordOptions)
 
 
+//let newPassword = "";
+
+/* Function to prompt user for password options
+//character types to include at least one character type: lowercase, uppercase, numeric and special characters
+//prompts
+//validate for each input
+passwordOptionsArray = passwordOptionsArray.concat(exampleArray);
+
+return passwordOptionsArray
+
+}
+
+*/
 // Function for getting a random element from an array
 function getRandom(arr) {
-  return 
-
+  return
+  
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
-let newPassword = "";
-
-let randomIndex = Math.floor(Math.random()*passwordsOptionArray.length)
-
-for (let i = 0; i < 64; i++) {
-
-    newPassword += ""
-
+  var passwordLength = prompt("Choose length of password length between 10 and 64 characters");
+  console.log(passwordLength)
   
-}
-return generatePassword;
+  if (passwordLength > 10 && passwordLength < 64) {
+    console.log("Yes")
+    var numericCharacters = confirm("I want password to have numeric characters")
+    console.log("Yes to numeric characters", numericCharacters)
+    var specialCharacters = confirm("Yes to special characters")
+    console.log("Yes to special character", specialCharacters)
+    var upperCasedCharacters = confirm("I want upper cased characters in my password")
+    console.log("Yes to upper cased charaters", upperCasedCharacters)
+    var lowerCasedCharacters = confirm("Yes to lower cased characters in my password")
+    console.log("Yes to lower cased characters", lowerCasedCharacters)
+    
+    
+    if (numericCharacters === true) {
+      passwordOptionsArray = passwordOptionsArray.concat(numericCharacter)
+      
+    }
+    
+    if (specialCharacters === true) {
+      passwordOptionsArray = passwordOptionsArray.concat(specialCharacter)
+      
+    }
+    
+    if (upperCasedCharacters === true) {
+      passwordOptionsArray = passwordOptionsArray.concat(upperCasedCharacter)
+      
+    }
+    
+    if (lowerCasedCharacters === true){
+      passwordOptionsArray = passwordOptionsArray.concat(lowerCasedCharacter)
+    }
+    
+    console.log(passwordOptionsArray)
+    
+    let randomIndex = Math.floor(Math.random() * passwordOptionsArray.length)
+  
+    for (let i = 0; i < passwordLength; i++) {
+  
+      //newPassword += ""
+  
+  
+      return "generate password"
+    }
+
+
+  }
+  
+  else {
+    console.log("Start again")
+    generatePassword()
+  }
+
 
 
 
 }
 
 // Get references to the #generate element
- generateBtn = document.querySelector('#generate');
+generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
